b66d14b790210dfd052b2e577a89806b19446b06
Ashmika <ashmika1194@gmail.com>
2023-05-05 21:24:16 +0530

    Server

--- models/connection.js
+++ models/connection.js
@@ -2,4 +2,4 @@ import mongoose from 'mongoose';
 const url="mongodb://127.0.0.1:27017/Packer_and_Mover";
 mongoose.set('strictQuery', true);
 mongoose.connect(url);
-console.log("Successfully connected to mongodb database server...");
+console.log("Successfully connected to mongodb database...");

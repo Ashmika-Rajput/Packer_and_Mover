048655a6be08870641e62c46c6910a2d9ed58e46
Ashmika <ashmika1194@gmail.com>
2023-05-12 19:32:39 +0530

    Category management api added

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import fileupload from 'express-fileupload';
 
 const app = express();
 
 //import routes for route level middleware
 import UserRouter from './routes/user.router.js'; 
+import CategoryRouter from './routes/category.router.js';
 
 //configuration to handle cross origin request
 app.use(cors());
+
+//configuration to handle req file data
+app.use(fileupload());
 
 //configuration to extract request body content
 app.use(bodyParser.json());
@@ -18,6 +23,7 @@ app.use(bodyParser.urlencoded({
 
 //to link route in route level middleware
 app.use("/user",UserRouter);
+app.use("/category",CategoryRouter);
 
 
 app.listen(3001);
